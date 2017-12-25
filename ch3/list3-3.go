1058a0622efa8f1237d9e12fc33695ff87cc54d4
y.urakawa <s08a2015@gmail.com>
2017-12-25 13:37:47 +0900

    [p70] ステータスコード等の取得

---
+++ ch3/list3-3.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "io/ioutil"
+    "log"
+    "net/http"
+)
+
+func main() {
+    resp, err := http.Get("http://localhost:18888")
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    log.Println(string(body))
+    log.Println("Status:", resp.Status)
+    log.Println("StatusCode:", resp.StatusCode)
+    log.Println("Headers:", resp.Header)
+    log.Println("Content-Length:", resp.Header.Get("Content-length"))
+}
+
