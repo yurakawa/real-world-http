ca1d71f45b0b0c1058e576c8f41810cb82b06a94
y.urakawa <s08a2015@gmail.com>
2017-12-25 13:31:42 +0900

    [p69] エラーチェックを省いた最小のコード

---
+++ ch3/list3-2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "io/ioutil"
+    "log"
+    "net/http"
+)
+
+func main() {
+    resp, _ := http.Get("http://localhost:18888")
+    defer resp.Body.Close()
+    body, _ := ioutil.ReadAll(resp.Body)
+    log.Println(string(body))
+}
+
