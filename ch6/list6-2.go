2495a88bb32521b70fe6f956238a74e78666d636
y.urakawa <s08a2015@gmail.com>
2018-01-08 01:52:15 +0900

    [p162] Go言語ではa宛先をhttps://にするだけでHTTPSが可能に

---
+++ ch6/list6-2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "io/ioutil"
+    "log"
+    "net/http"
+)
+
+func main() {
+    resp, err := http.Get("https://example.com")
+    if err != nil {
+        // エラー発生
+        panic(err)
+    }
+    // このスコープが抜けたところで必ずクローズ
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body) // ioutil.ReadAllでサーバレスポンスを最後まで一括で読み切る
+    if err != nil {
+        panic(err)
+    }
+    log.Println(string(body))
+}
+
